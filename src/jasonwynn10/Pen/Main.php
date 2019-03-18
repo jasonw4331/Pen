d497a5e8ef951fc2d9dc179560c50591f7085e1d
jasonwynn10 <jasonwynn10@me.com>
2019-03-18 17:08:19 -0400

    Stream 2 Mid

--- src/jasonwynn10/Pen/Main.php
+++ src/jasonwynn10/Pen/Main.php
@@ -11,15 +11,22 @@ namespace jasonwynn10\Pen;
 If I use a UI, then it will be dependant on an API from a virion or other plugin but if I use a sign interface, the plugin must hook on datapackets directly
 */
 
+use jojoe77777\FormAPI\SimpleForm;
+use pocketmine\event\inventory\InventoryCloseEvent;
 use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\inventory\ShapedRecipe;
+use pocketmine\inventory\transaction\action\SlotChangeAction;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIds;
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
 use pocketmine\plugin\PluginBase;
 use pocketmine\resourcepacks\ZippedResourcePack;
 
 class Main extends PluginBase implements Listener {
+	/** @var array[] $data */
+	protected $data = [];
 	public function onLoad() {
 		$manager = $this->getServer()->getResourcePackManager();
 		$pack = new ZippedResourcePack($this->getFile()."resources/PenPlugin.mcpack");
@@ -37,6 +44,10 @@ class Main extends PluginBase implements Listener {
 		$currentUUIDPacks = $property->getValue($manager);
 		$currentUUIDPacks[strtolower($pack->getPackId())] = $pack;
 		$property->setValue($manager, $currentUUIDPacks);
+
+		$property = $reflection->getProperty("serverForceResources");
+		$property->setAccessible(true);
+		$property->setValue($manager, true);
 	}
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -73,11 +84,63 @@ class Main extends PluginBase implements Listener {
 		}
 	}
 
+	/**
+	 * @param InventoryTransactionEvent $event
+	 */
 	public function onTransaction(InventoryTransactionEvent $event) {
-		foreach($event->getTransaction()->getInventories() as $inventory) {
-			if($inventory->getName() === "Pen") {
-				// TODO:
+		$rewrite = null;
+		$slot = null;
+		$inventory = null;
+		foreach($event->getTransaction()->getActions() as $action) {
+			if($action instanceof SlotChangeAction and $action->getInventory() instanceof PenInventory) {
+				$rewrite = clone $action->getSourceItem();
+				$slot = $action->getSlot();
+				$inventory = $action->getInventory();
+				break;
 			}
+		}
+		if($rewrite !== null and $slot !== null) {
+			$event->getTransaction()->getSource()->removeWindow($inventory, true);
+			$this->data[$event->getTransaction()->getSource()->getName()] = [$rewrite, $slot];
+		}
+	}
+
+	/**
+	 * @param InventoryCloseEvent $event
+	 */
+	public function onInvClose(InventoryCloseEvent $event) {
+		if($event->getInventory() instanceof PenInventory) {
+			$player = $event->getPlayer();
+			$form = new SimpleForm(function($data){
+				var_dump($data);
+				/*
+				$pk = new InventoryTransactionPacket();
+				$pk->transactionType = InventoryTransactionPacket::TYPE_USE_ITEM;
+				$pk->actions = [];
+				$pk->trData = new \stdClass();
+				$pk->trData->actionType = InventoryTransactionPacket::USE_ITEM_ACTION_CLICK_BLOCK;
+				$pk->trData->x = $player->getFloorX();
+				$pk->trData->y = $player->getFloorY() + ceil($player->height);
+				$pk->trData->z = $player->getFloorZ();
+				$pk->trData->face = Vector3::SIDE_UP;
+				$pk->trData->hotbarSlot = $player->getInventory()->getHeldItemIndex();
+				$pk->trData->itemInHand = ItemFactory::get(ItemIds::SIGN);
+				$pk->trData->playerPos = $event->getPlayer()->asVector3();
+				$pk->trData->clickPos = new Vector3(); // different from x/y/z
+				$player->sendDataPacket($pk);
+				*/
+			});
+			$form->setTitle("Pen Selection");
+			$form->addButton("Write Enchantment");
+			$form->addButton("Write Lore");
+			$player->sendForm($form);
+		}
+	}
+
+	public function onPacket(DataPacketReceiveEvent $event) {
+		if($event->getPacket() instanceof InventoryTransactionPacket) {
+			//var_dump($event->getPacket());
+			echo ":P\n";
 		}
 	}
 }
